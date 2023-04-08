64d250bc86fd4605a647191f45de81e57cfbe616
selamawit siferh <siferhselamawit@gmail.com>
2023-04-08 12:03:33 +0300

    add home screen file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import data from "./data";
 import{BrowserRouter, Route,Routes} from"react-router-dom";
 import "./screens/HomeScreen"
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -12,7 +13,19 @@ function App() {
           Soliana Tibeb
         </a>
       </header>
-      <main><h1> Featured Products</h1><Routes><Route path="/" element={<HomeScreen />}/> </Routes>
+      <main>
+        
+        
+      <Routes> 
+        <Route path='/' element = {<HomeScreen />} />
+        {/* <Route path='/About Us' element = {<AboutusComponent/>} />
+        <Route path='/Contact Us' element = {<ContactusComponent/>} />
+        <Route path='/History' element = {<HistoryComponent/>} />
+        <Route path='/Home' element = {<HomeComponent />} />
+        <Route path='/Home' element = {<AboutusComponent />} />
+        <Route path='/SignUP' element = {<SignupComponent />} /> */}
+      </Routes>
+        <h1> Featured Products</h1>
     <div className="products">  { data.products.map(product=>(<div className="product " key={product.slug}>
       <a href={`/product/${product.slug}`}><img src={product.Image}atl={product.name}></img>
       </a>
